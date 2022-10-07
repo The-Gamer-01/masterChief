3bc533cc8c05a03c81182a404b0570062fe30139
绿名蒟蒻 <843497509@qq.com>
2022-10-08 02:30:32 +0800

    更新网关模块注释用以满足CI

--- masterChief-gateway/src/main/java/com/hyx/GatewayApplication.java
+++ masterChief-gateway/src/main/java/com/hyx/GatewayApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
+ * 网关模块启动类.
+ *
  * @author hyx
  **/
 
